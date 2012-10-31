cf988ccdda3522e3577e1e6771e8415041d980e5
Вахотин Игорь <vahotin@bars-open.ru>
2012-10-31 15:12:49 +0400

    Conversion into setuptools

---
+++ setup.py
@@ -0,0 +1,27 @@
+#coding: utf-8
+from setuptools import setup, find_packages
+
+requires = []
+with open('src/requires.txt', 'r') as f:
+    requires.extend(f.readlines())
+
+setup(name='m3_audit',
+      version='1.1',
+      url='https://src.bars-open.ru/py/m3/m3_contrib/m3_audit',
+      license='Apache License, Version 2.0',
+      author='BARS Group',
+      author_email='telepenin@bars-open.ru',
+      description=u'Аудит операций в прикладной системе',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      install_requires=requires,
+      include_package_data=True,
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Natural Language :: Russian',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+      )
